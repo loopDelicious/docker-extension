991551e811fdda83d468fdec2df5a26a8beede9f
Joyce <joycelin79@gmail.com>
2022-09-27 16:38:16 -0700

    update header to match side navigation

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Header = () => (
     <>
-        <Typography variant="h3">Postman Results</Typography>
+        <Typography variant="h3">Newman</Typography>
         <Typography variant="body1" color="text.secondary" sx={{ marginY: 2 }}>
             This desktop extension displays output from a Postman collection run.
         </Typography>
